da68c53298f3632f160599fbbc1c03ce9b4dd33c
Ivasin Vitaliy <IvashinVitaliy1989@gmail.com>
2021-12-09 14:12:03 +0200

    4 lesson

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 
-const Categories = ({items, onClick}) => {
-    const [activeItem, setActiveItem] = useState(0)
+const Categories = ({ items, onClick }) => {
+    const [ activeItem, setActiveItem ] = useState(0)
+
+
     const list = items?.map((item, i) =>
         <li className={ activeItem === i ? 'active' : '' }
             onClick={ () => setActiveItem(i) }
             key={ `${ item }+${ i }` }>{ item }
         </li>)
+
     return (
         <div className="categories">
             <ul>
---
+++ src/test/index.js
@@ -0,0 +1,6 @@
+const ul = document.querySelector('.ul-list');
+console.log(ul)
+
+ul.addEventListener('click', function () {
+    console.log(this)
+})
